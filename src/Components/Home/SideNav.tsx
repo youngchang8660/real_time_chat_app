5f7c0437a62d9fc7d4056ee6c4a3ed9334d6849c
youngchang8660 <youngchang8660@gmail.com>
2021-11-01 20:49:22 -0400

    login and signup

---
+++ src/Components/Home/SideNav.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+
+
+class SideNav extends React.Component<any, {
+
+}>{
+    constructor(props: any) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    componentDidMount = () => {
+
+    }
+}
+
+export default SideNav;
