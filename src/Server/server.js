5087f716f2bd1970203b49a5ca3b803bb417b41c
youngchang8660 <youngchang8660@gmail.com>
2022-02-02 18:25:08 -0500

    build and customize app title

--- src/Server/server.js
+++ src/Server/server.js
@@ -6,6 +6,10 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const cors = require('cors');
 const server = http.createServer(app);
+const path = require('path');
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 const socketIo = require("socket.io");
 const io = socketIo(server, {
     cors: {
