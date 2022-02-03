64d6a3a19e14f653adb61d985c0cf9e34eb413ed
youngchang8660 <youngchang8660@gmail.com>
2022-02-02 19:03:31 -0500

    build

--- src/Server/server.js
+++ src/Server/server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const server = http.createServer(app);
 const path = require('path');
 app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../build/index.html'));
+    res.sendFile(path.join(__dirname, '../../build/index.html'));
 });
 const socketIo = require("socket.io");
 const io = socketIo(server, {
