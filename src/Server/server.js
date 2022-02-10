7cc37f3b09e760052e17476cd5b5298f07331d34
youngchang8660 <youngchang8660@gmail.com>
2022-02-09 22:53:03 -0500

    express static

--- src/Server/server.js
+++ src/Server/server.js
@@ -6,11 +6,14 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const cors = require('cors');
 const server = http.createServer(app);
-// const path = require('path');
+const path = require('path');
 app.use(cors());
 app.use(express.json({limit: '50mb'}))
+app.use(express.static(`${__dirname}/../build`));
 
-// app.use(express.static(__dirname + '../../build'))
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../../build/index.html'));
+// })
 // app.get('*', (req, res) => {
 //     res.sendFile(path.join(__dirname, '../../build/index.html'))
 // })
